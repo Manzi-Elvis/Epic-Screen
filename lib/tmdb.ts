2673183c16e0b46521090a6b558fd2dac4f45c03
MANZI RURANGIRWA Elvis <elvismanzi16@gmail.com>
2025-09-22 08:54:47 +0200

    ...

--- lib/tmdb.ts
+++ lib/tmdb.ts
@@ -1,7 +1,6 @@
 const TMDB_API_KEY = process.env.TMDB_API_KEY 
 const TMDB_BASE_URL = "https://api.themoviedb.org/3"
 const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p"
-
 export interface Movie {
   id: number
   title: string
@@ -18,7 +17,6 @@ export interface Movie {
   popularity: number
   video: boolean
 }
-
 export interface MovieDetails extends Movie {
   genres: Genre[]
   runtime: number
